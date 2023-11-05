f48963bea8a39d48050caf54ef3571f62dbd2d0f
teejaynam <teejaynam@gmail.com>
2023-11-05 16:28:25 -0500

    add task 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,34 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee < ActiveRecord::Base
+    belongs_to :store
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+
+    validates :store, presence: true
+end
+
+class Store < ActiveRecord::Base
+    has_many :employees
+
+    validates :name, length: { minimum: 3 }
+    validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+    validate :must_carry_apparel
+end
+
+puts "enter store name"
+store_name = gets.chomp
+
+store = Store.new(name: store_name)
+
+if store.save
+else
+    puts 'Store could not be saved:'
+    store.errors.full_messages.each do |message|
+        puts message
+    end
+end
