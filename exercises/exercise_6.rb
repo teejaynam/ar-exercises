c861ca5e0937d88a6ef1ebb9835e5223e32bc511
teejaynam <teejaynam@gmail.com>
2023-11-05 16:23:53 -0500

    add task 6, one to many associations

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,17 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+class Store < ActiveRecord::Base
+    has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+    belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Buddy", last_name: "Holly", hourly_rate: 70)
+
+@store2.employees.create(first_name: "Wombo", last_name: "Combo", hourly_rate: 60)
